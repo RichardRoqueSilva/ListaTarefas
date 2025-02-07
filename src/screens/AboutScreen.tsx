90668d146b45d04dfd6496f2d6a1d35e080c65fc
Richard Roque <richard.roque@castgroup.com.br>
2025-02-07 10:52:24 -0300

    Alterando o About

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,65 +1,64 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 
 const AboutScreen: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.contentContainer}>
-        <View style={styles.textContainer}>
-          <Text style={[styles.description, styles.firstParagraph]}>
-            Organize sua vida com facilidade! Este aplicativo de lista de tarefas é a ferramenta perfeita para adicionar, remover e acompanhar suas atividades diárias.
-          </Text>
-          <Text style={[styles.description, styles.secondParagraph]}>
-            Mantenha-se <Text style={styles.highlightedText}>produtivo</Text> e <Text style={styles.highlightedText}>eficiente</Text>, simplificando sua rotina e focando no que realmente importa.
-          </Text>
-        </View>
-        <View style={styles.imageContainer}>
-          <Image
-            style={styles.image}
-            source={require('../../assets/images/agenda.png')}
-          />
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <View style={styles.contentContainer}>
+          <View style={styles.textContainer}>
+            <Text style={[styles.description, styles.firstParagraph]}>
+              Organize sua vida com facilidade! Este aplicativo de lista de tarefas é a ferramenta perfeita para adicionar, remover e acompanhar suas atividades diárias.
+            </Text>
+            <Text style={[styles.description, styles.secondParagraph]}>
+              Mantenha-se <Text style={styles.highlightedText}>produtivo</Text> e <Text style={styles.highlightedText}>eficiente</Text>, simplificando sua rotina e focando no que realmente importa.
+            </Text>
+          </View>
+          <View style={styles.imageContainer}>
+            <Image
+              style={styles.image}
+              source={require('../../assets/images/agenda.png')}
+            />
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1, // Permite que o conteúdo cresça e role
+  },
   container: {
     flex: 1,
     padding: 20,
     backgroundColor: '#D2B48C', // Marrom claro (Tan)
   },
   contentContainer: {
-    flexDirection: 'row',
-    alignItems: 'flex-start',
-    justifyContent: 'space-between',
+    flexDirection: 'column', // Alterado para 'column'
+    alignItems: 'center',
+    justifyContent: 'flex-start', // Alterado para 'flex-start'
     marginTop: 40,
     paddingHorizontal: 30,
   },
   textContainer: {
-    flex: 1,
-    marginRight: 20,
-    alignItems: 'flex-start',
-    maxWidth: '50%',
-    paddingLeft: 40,
-    marginTop: 30,
+    width: '100%', // Ocupa a largura total
+    marginBottom: 20, // Adiciona espaço abaixo do texto
+    alignItems: 'center',  // Centraliza o texto
   },
   imageContainer: {
     alignItems: 'center',
-    marginTop: 20,
-    marginRight: 200, // Adiciona espaçamento à direita
   },
   image: {
     width: 400,
     height: 400,
   },
   description: {
-    textAlign: 'left',
+    textAlign: 'center',   //Centraliza o texto
     lineHeight: 40,
     marginBottom: 10,
-    marginTop: 5,
-    marginLeft: 200,
+    marginTop: 1,
   },
   firstParagraph: {
     fontSize: 22,
